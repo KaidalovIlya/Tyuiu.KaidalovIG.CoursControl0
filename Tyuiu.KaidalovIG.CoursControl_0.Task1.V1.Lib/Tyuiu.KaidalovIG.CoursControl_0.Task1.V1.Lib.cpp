0fe8afa018ccd4f6540956e5d1920a84bf1d0e7b
Tyuiu.KaidalovIG@outlook.com <Kaydalov.2026@mail.ru>
2023-09-21 12:15:19 +0500

    Add project files.

---
+++ Tyuiu.KaidalovIG.CoursControl_0.Task1.V1.Lib/Tyuiu.KaidalovIG.CoursControl_0.Task1.V1.Lib.cpp
@@ -0,0 +1,19 @@
+﻿// Tyuiu.KaidalovIG.CoursControl_0.Task1.V1.Lib.cpp : Defines the functions for the static library.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "../Tyuiu.CoursControl_0/Tyuiu.CoursControl_0.cpp"
+
+
+class Service : public ISprint0Task8V0
+{
+	virtual int Control(int a) override
+	{
+		int num1 = a / 100;
+		int num2 = a / 10 % 10;
+		int num3 = a % 10;
+		return num1 * num2 * num3;
+	}
+
+};
---
+++ Tyuiu.KaidalovIG.CoursControl_0.Task1.V1.Test/Tyuiu.KaidalovIG.CoursControl_0.Task1.V1.Test.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Tyuiu.KaidalovIG.CoursControl_0.Task1.V1.Lib/Tyuiu.KaidalovIG.CoursControl_0.Task1.V1.Lib.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TyuiuKaidalovIGCoursControl0Task1V1Test
+{
+	TEST_CLASS(TyuiuKaidalovIGCoursControl0Task1V1Test)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			ISprint0Task8V0* rezult = new Service();
+			int a = 123;
+			int b = rezult->Control(a);
+			Assert::AreEqual(6, b);
+		}
+	};
+}
